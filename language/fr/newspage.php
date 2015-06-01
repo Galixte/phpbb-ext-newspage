33d2a5114d46592fbe97aadfbb9e871714ae0682
Raphaël M. <Galixte@users.noreply.github.com>
2015-06-01 07:45:57 +0200

    Update for FRENCH translation for Newspage extension
    - update for newspage.php file.

--- language/fr/newspage.php
+++ language/fr/newspage.php
@@ -11,14 +11,34 @@
  * the license.txt file.
  */
 
+/**
+* DO NOT CHANGE
+*/
 if (!defined('IN_PHPBB'))
 {
 	exit;
 }
+
 if (empty($lang) || !is_array($lang))
 {
 	$lang = array();
 }
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
 
 $lang = array_merge($lang, array(
 	'NEWS'						=> 'Nouvelles du forum',
@@ -43,7 +63,7 @@ $lang = array_merge($lang, array(
 	'NEWS_NUMBER'				=> 'Nouvelles par page',
 	'NEWS_PAGES'				=> 'Nombre de pages',
 	'NEWS_POLL'					=> 'Sondage ',
-	'NEWS_POLL_GOTO'			=> 'Cliquer ici pour voter !',
+	'NEWS_POLL_GOTO'			=> 'Cliquer ici pour voter !',
 	'NEWS_POST_BUTTONS'			=> 'Afficher les boutons liés aux messages',
 	'NEWS_POST_BUTTONS_EXPLAIN'	=> 'Citer, modifier, etc.',
 	'NEWS_READ_FULL'			=> 'Lecture complète de la nouvelle',
